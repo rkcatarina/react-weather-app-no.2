e34167bef3f455de8d503b9711d9ac41f2585325
CatarinaRK <79782399+rkcatarina@users.noreply.github.com>
2021-05-17 11:28:51 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
-import './App.css';
+import React from "react";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        This project was coded by Catarina Redshaw Kranich and is
+        <a
+          href="https://github.com/rkcatarina/react-weather-app-no.2"
+          target="_blank"
+        > open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
-
-
