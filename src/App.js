77b054b942e65551873296c94606a57ea8fd80c1
CatarinaRK <79782399+rkcatarina@users.noreply.github.com>
2021-05-17 16:54:36 +0200

    Added CSS styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
-        Weather App
-        <footer>
-          This project was coded by Catarina Redshaw Kranich and is
-          <a
-            href="https://github.com/rkcatarina/react-weather-app-no.2"
-            target="_blank"
-          >
-            {" "}
-            open-sourced on GitHub
-          </a>
-        </footer>
-      </div>
+      
+      <h1>Weather App</h1>
+      <Weather />
+      <footer>
+        This project was coded by Catarina Redshaw Kranich and is
+        <a
+          href="https://github.com/rkcatarina/react-weather-app-no.2"
+          target="_blank"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
