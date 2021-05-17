7b3de4216279fcedb2d27d028d7e9694f5cce6be
CatarinaRK <79782399+rkcatarina@users.noreply.github.com>
2021-05-17 11:53:23 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Catarina Redshaw Kranich and is
-        <a
-          href="https://github.com/rkcatarina/react-weather-app-no.2"
-          target="_blank"
-        > open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by Catarina Redshaw Kranich and is
+          <a
+            href="https://github.com/rkcatarina/react-weather-app-no.2"
+            target="_blank"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
